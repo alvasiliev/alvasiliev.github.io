ec59367444d85246b49e6c69001a31985f840789
Alex V Vasiliev <al.v.vasiliev@gmail.com>
2020-01-02 16:28:03 +0300

    Implement asteroids and collisions

--- app.js
+++ app.js
@@ -62,6 +62,21 @@ class Rect {
     }
 }
 
+class Circle {
+    constructor(radius, color, position) {
+        this.radius = radius;
+        this.color = color;
+        this.position = position;
+    }
+
+    draw(context) {
+        context.fillStyle = this.color;
+        context.beginPath();
+        context.arc(this.position.x, this.position.y, this.radius, 0, 2 * Math.PI, false);
+        context.fill();
+    }
+}
+
 // Engines
 
 class Figures {
@@ -184,9 +199,6 @@ class PhysicsEngine {
                 this.moveGameObject(f.getGameObject(), timePassed);
             }
         }
-
-        const figuresWithoutDestroyed = this.figures.get().filter(f => !f.getGameObject || !f.getGameObject().getIsDestroyed());
-        this.figures.set(figuresWithoutDestroyed);
     }
 
     moveGameObject(gameObject, timePassed) {
@@ -225,11 +237,88 @@ class PhysicsEngine {
     }
 }
 
+class CollisionEngine {
+    constructor(figures) {
+        this.figures = figures;
+    }
+
+    checkShipCollisions() {
+        const ship = this.figures.get().find(f => f instanceof Ship);
+        const asteroids = this.figures.get().filter(f => f instanceof Asteroid && !f.getGameObject().getIsDestroyed());
+
+        for (let a of asteroids) {
+            if (!a.getGameObject().getIsDestroyed()) {
+                const isCollision = this.checkCollision(ship, a);
+                if (isCollision) return true;
+            }
+        }
+        return false;
+    }
+
+    checkMissleCollisions() {
+        const missles = this.figures.get().filter(f => f instanceof Missle && !f.getGameObject().getIsDestroyed());
+        const asteroids = this.figures.get().filter(f => f instanceof Asteroid && !f.getGameObject().getIsDestroyed());
+        const newAsteroids = [];
+
+        for (let a of asteroids) {
+            for (let m of missles) {
+                const isCollision = this.checkCollision(m, a);
+                if (isCollision) {
+                    m.getGameObject().destroy();
+                    const wreckles = this.blowUpAsteroid(a);
+                    if (wreckles.length) {
+                        newAsteroids.push(...wreckles);
+                    }
+                }
+            }
+        }
+
+        return newAsteroids;
+    }
+
+    checkCollision(go1, go2) {
+        const go1Radius = go1.getGameObject().width / 2;
+        const go1X = go1.getGameObject().x;
+        const go1Y = go1.getGameObject().y;
+
+        const go2Radius = go2.getGameObject().width / 2;
+        const go2X = go2.getGameObject().x;
+        const go2Y = go2.getGameObject().y;
+
+        const minDistance = (go1Radius + go2Radius);
+        const minDistance2 = minDistance * minDistance;
+
+        const dx = go1X - go2X;
+        const dy = go1Y - go2Y;
+        const distance2 = dx * dx + dy * dy;
+
+        return distance2 < minDistance2;
+    }
+
+    blowUpAsteroid(a) {
+        a.getGameObject().destroy();
+
+        const wreckles = [];
+        if (a.radius > 20) {
+            const wreckleRadius = a.radius / 3;
+            const wreckleSpeed = a.speed * 1.5;
+            const x = a.getGameObject().x;
+            const y = a.getGameObject().y;
+
+            wreckles.push(new Asteroid(x, y, Math.random() * 360 * Math.PI / 180, wreckleRadius, wreckleSpeed));
+            wreckles.push(new Asteroid(x, y, Math.random() * 360 * Math.PI / 180, wreckleRadius, wreckleSpeed));
+            wreckles.push(new Asteroid(x, y, Math.random() * 360 * Math.PI / 180, wreckleRadius, wreckleSpeed));
+        }
+
+        return wreckles;
+    }
+}
+
 // Objects
 
 class Background {
     constructor(width, height) {
-        this.drawItem = new Rect(width, height, '#bbb');
+        this.drawItem = new Rect(width, height, '#78f');
     }
 
     getDrawItem() {
@@ -288,27 +377,94 @@ class Missle {
     }
 }
 
+class Asteroid {
+    constructor(x, y, angle, radius, speed) {
+        this.radius = radius;
+        this.width = radius * 2;
+        this.height = radius * 2;
+        this.speed = speed;
+        this.gameObject = new GameObject(x, y, this.width, this.height, angle, this.speed, 0, false);
+        this.drawItem = new Circle(this.radius, '#555', this.gameObject);
+        //this.drawItem = new Sprite('asteroid.png', this.width, this.height, this.gameObject);
+    }
+
+    getDrawItem() {
+        return this.drawItem;
+    }
+
+    getGameObject() {
+        return this.gameObject;
+    }
+}
+
 // Game
 
 class Game {
     constructor() {
         this.intervalHandle = null;
         this.calcFrequency = 60; // 60 раз в секунду
+        this.isFinished = false;
 
         this.keys = new Keys();
-
         this.figures = new Figures();
 
-        const width = 800;
-        const height = 600;
-        this.renderEngine = new RenderEngine(width, height, this.figures);
-        this.physicsEngine = new PhysicsEngine(width, height, this.figures);
+        this.width = 800;
+        this.height = 600;
+        this.renderEngine = new RenderEngine(this.width, this.height, this.figures);
+        this.physicsEngine = new PhysicsEngine(this.width, this.height, this.figures);
+        this.collisionEngine = new CollisionEngine(this.figures);
 
-        this.background = new Background(width, height);
+        this.background = new Background(this.width, this.height);
+        this.ship = new Ship(this.width / 2, this.height / 2, 0);
+
+        this.init();
+    }
+
+    init() {
+        this.background.getDrawItem().color = '#78f';
+        this.figures.set([]);
+
+        const asteroids = this.generateAsteroids();
         this.addFigure(this.background);
-
-        this.ship = new Ship(width / 2, height / 2, 0);
         this.addFigure(this.ship);
+        asteroids.forEach(a => this.addFigure(a));
+    }
+
+    generateAsteroids() {
+        const asteroids = [];
+        const asteroidsCount = 5;
+        const minRadius = 20;
+        const maxRadius = 70;
+        const minSpeed = 1;
+        const maxSpeed = 20
+        for (let i = 0; i < asteroidsCount; ++i) {
+            const rand = Math.random();
+            const radius = Math.trunc(rand * (maxRadius - minRadius) + minRadius);
+            const speed = (maxSpeed - minSpeed) * (1 - rand) + minSpeed;
+
+            const linearPos = Math.trunc((this.width + this.height) * 2 * Math.random());
+
+            let x, y;
+            if (linearPos < this.width) {
+                x = linearPos;
+                y = 0;
+            } else if (linearPos < this.width + this.height) {
+                x = this.width;
+                y = linearPos - this.width;
+            } else if (linearPos < this.width * 2 + this.height) {
+                x = linearPos - this.width - this.height;
+                y = this.height;
+            } else {
+                x = 0;
+                y = linearPos - this.width * 2 - this.height;
+            }
+
+            const angle = Math.random() * 360 * Math.PI / 180;
+
+            const a = new Asteroid(x, y, angle, radius, speed);
+            asteroids.push(a);
+        }
+        return asteroids;
     }
 
     addFigure(f) {
@@ -343,14 +499,51 @@ class Game {
     }
 
     performAll() {
-        this.processUserControl();
-        this.physicsEngine.moveAll();
+        if (!this.isFinished) {
+            this.processUserControl();
+            this.physicsEngine.moveAll();
+        }
         this.renderEngine.drawFrame();
+
+        if (!this.isFinished) {
+            const newAsteriods = this.collisionEngine.checkMissleCollisions();
+            for (let a of newAsteriods) {
+                this.addFigure(a);
+            }
+
+            // Remove all destroyed objects
+            const figuresWithoutDestroyed = this.figures.get().filter(f => !f.getGameObject || !f.getGameObject().getIsDestroyed());
+            this.figures.set(figuresWithoutDestroyed);
+
+            const isShipCollision = this.collisionEngine.checkShipCollisions();
+            if (isShipCollision) {
+                this.loose();
+            } else {
+                const asteroids = this.figures.get().filter(f => f instanceof Asteroid);
+                if (asteroids.length === 0) {
+                    this.win();
+                }
+            }
+        }
+    }
+
+    loose() {
+        this.background.getDrawItem().color = '#f78';
+        this.isFinished = true;
+    }
+
+    win() {
+        this.background.getDrawItem().color = '#7f8';
+        this.isFinished = true;
     }
 
     start() {
         if (!this.intervalHandle) {
             this.intervalHandle = setInterval(() => this.performAll(), 1000 / this.calcFrequency);
+        }
+        if (this.isFinished) {
+            this.isFinished = false;
+            this.init();
         }
     }
 
